7c8033ebcbe4354b112dfc74c1e5330a1b6132f2
HasnainShahidToor2907 <80328277+HasnainShahidToor2907@users.noreply.github.com>
2022-04-03 14:42:00 +0500

    Add files via upload

---
+++ scripts/cameramove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramove : MonoBehaviour
+{
+    public Transform myplayerhead;
+    public float startFOV, targetFOV, FOVSpeed;
+    public Camera myCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCamera = FindObjectOfType<Camera>();
+        startFOV = myCamera.fieldOfView;
+        targetFOV = startFOV;
+
+    }
+
+    private void LateUpdate()
+    {
+        transform.rotation = myplayerhead.transform.rotation;
+        transform.position = myplayerhead.transform.position;
+        transform.localScale = myplayerhead.transform.localScale;
+
+        myCamera.fieldOfView = Mathf.Lerp(startFOV, targetFOV, FOVSpeed * Time.deltaTime);
+
+    }
+
+    public void zoomOut()
+    {
+        targetFOV = startFOV;
+    }
+
+    public void zoomIn(float a)
+    {
+        targetFOV = a;
+    }
+   
+}
---
+++ scripts/enemyScript.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyScript : MonoBehaviour
+{
+    NavMeshAgent myAgent;
+    public LayerMask ground;
+    Vector3 destinationPoint;
+    bool destinationSet;
+    float destinationRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<NavMeshAgent>();
+        myAgent = FindObjectOfType<NavMeshAgent>();
+        destinationRange = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        guarding();
+    }
+
+    private void guarding()
+    {
+        if (!destinationSet)
+        {
+            float tempxValue = Random.Range(-destinationRange, destinationRange); 
+            float tempzValue = Random.Range(-destinationRange, destinationRange);
+
+            destinationPoint = new Vector3(transform.position.x + tempxValue, transform.position.y, transform.position.z + tempzValue);
+            destinationSet = true;
+        }
+        else
+        {
+            myAgent.SetDestination(destinationPoint);
+            destinationSet = false;
+        }
+    }
+}
